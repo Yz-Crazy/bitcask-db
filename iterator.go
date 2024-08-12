3eff21aa13f89785a6d05137e8a6b103a717d728
Yz-Crazy <474335897@qq.com>
2024-08-12 14:43:05 +0800

    feat add iterator interface

--- iterator.go
+++ iterator.go
@@ -1,10 +1,76 @@
 package bitcask_db
 
-import "bitcask-db/index"
+import (
+	"bitcask-db/index"
+	"bytes"
+)
 
 // Iterator 迭代器
 type Iterator struct {
 	indexIter index.Iterator
 	db        *DB
 	Options   IteratorOptions
+}
+
+func (db *DB) NewIterator(opts IteratorOptions) *Iterator {
+	indexIter := db.index.Iterator(opts.Reverse)
+	return &Iterator{
+		indexIter: indexIter,
+		db:        db,
+		Options:   opts,
+	}
+}
+
+// Rewind 重新回到迭代器的起点，即第一个数据
+func (it *Iterator) Rewind() {
+	it.indexIter.Rewind()
+	it.skipToNext()
+}
+
+// Seek 根据传入的 key 查找到第一个大于（或小于）等于的目标 key，根据从这个 key 开始遍历
+func (it *Iterator) Seek(key []byte) {
+	it.indexIter.Seek(key)
+	it.skipToNext()
+}
+
+// Next 跳转到下一个 key
+func (it *Iterator) Next() {
+	it.indexIter.Next()
+	it.skipToNext()
+}
+
+// Valid 是否有效，既是否已经遍历完了所有的 key，用于退出遍历
+func (it *Iterator) Valid() bool {
+	return it.indexIter.Valid()
+}
+
+// Key 当前遍历位置的 Key 数据
+func (it *Iterator) Key() []byte { return it.indexIter.Key() }
+
+// Value 当前遍历位置的 Value 数据
+func (it *Iterator) Value() ([]byte, error) {
+	defer it.db.mu.RUnlock()
+	it.db.mu.RLock()
+	logRecordPos := it.indexIter.Value()
+	return it.db.getValueByPosition(logRecordPos)
+}
+
+// Close 关闭迭代器，释放相应资源
+func (it *Iterator) Close() {
+	it.indexIter.Close()
+}
+
+func (it *Iterator) skipToNext() {
+	prefixLen := len(it.Options.Prefix)
+	if prefixLen == 0 {
+		return
+	}
+
+	for ; it.indexIter.Valid(); it.indexIter.Next() {
+		key := it.indexIter.Key()
+		if prefixLen <= len(key) && bytes.Compare(key[:prefixLen], it.Options.Prefix) == 0 {
+			break
+		}
+	}
+
 }
